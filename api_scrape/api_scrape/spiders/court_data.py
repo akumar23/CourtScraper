e2ba9638bd4dcaf9f0bfb0758ff5acc7a49ad2f3
Aryan Kumar <kumar.aryan@gmail.com>
2022-08-28 12:39:50 -0700

    changed spider to only output results to csv

--- api_scrape/api_scrape/spiders/court_data.py
+++ api_scrape/api_scrape/spiders/court_data.py
@@ -4,10 +4,10 @@ import json
 class CourtDataSpider(scrapy.Spider):
     name = 'court-data'
     allowed_domains = ['webapps.sftc.org']
-    start_urls = ['https://webapps.sftc.org/ci/CaseInfo.dll/datasnap/rest/TServerMethods1/GetCasesWithFilings/2022-08-26/49F92C8A8BDE62822F30E5471C478E65CE4C0711/']
+    start_urls = ['https://webapps.sftc.org/ci/CaseInfo.dll/datasnap/rest/TServerMethods1/GetCasesWithFilings/2022-08-26/30307C63056FAB05FC6E750B80A9B8DDA3A55D87/']
 
     def parse(self, response):
         resp = json.loads(response.body)
         yield {
-            "Data" : resp
+            "Data" : resp['result']['CASE_NUMBER']
         }
