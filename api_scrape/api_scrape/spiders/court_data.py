d9cfd6d2fde81acd5d2dcd607dd003230ad2d5a9
Aryan Kumar <kumar.aryan@gmail.com>
2022-08-28 18:07:13 -0700

    extracted just the court relevant data from json

--- api_scrape/api_scrape/spiders/court_data.py
+++ api_scrape/api_scrape/spiders/court_data.py
@@ -4,10 +4,12 @@ import json
 class CourtDataSpider(scrapy.Spider):
     name = 'court-data'
     allowed_domains = ['webapps.sftc.org']
-    start_urls = ['https://webapps.sftc.org/ci/CaseInfo.dll/datasnap/rest/TServerMethods1/GetCasesWithFilings/2022-08-26/30307C63056FAB05FC6E750B80A9B8DDA3A55D87/']
+    start_urls = ['https://webapps.sftc.org/ci/CaseInfo.dll/datasnap/rest/TServerMethods1/GetCasesWithFilings/2022-08-26/46506EA6633D5024DBAA8DA85C67E6F43339B429/']
 
     def parse(self, response):
         resp = json.loads(response.body)
+        court_data = json.dumps(resp['result'][1])['CASE_NUMBER']
+
         yield {
-            "Data" : resp['result']['CASE_NUMBER']
+            "Case Number" : court_data
         }
